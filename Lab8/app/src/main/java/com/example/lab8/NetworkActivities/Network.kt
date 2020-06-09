b15c4a8b57cb2d427846501695ce8541d82ba52a
Javier Salazar <40800436+zombiewafle@users.noreply.github.com>
2020-06-08 22:00:17 -0600

    Subí el archivo que no era.

---
+++ Lab8/app/src/main/java/com/example/lab8/NetworkActivities/Network.kt
@@ -0,0 +1,8 @@
+package com.example.lab8.NetworkActivities
+
+data class Network(
+    val id: Int,
+    val name: String,
+    val descp: String,
+    val url: String
+)
---
+++ Lab8/app/src/main/java/com/example/lab8/NewsActivities/NewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lab8.NewsActivities
+
+import androidx.lifecycle.ViewModel
+
+class NewsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
